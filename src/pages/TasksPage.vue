ca9940c7694ced536fd8246838bbefe4ffb9d250
Andrey Grytsenko <andreyapg@gmail.com>
2023-09-16 10:46:57 +0300

    31 Using computed properties for displaying completed & uncompleted tasks 2023-09-16

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -9,12 +9,13 @@
                             placeholder="+ Add new task. Press enter to save." />
                     </div>
 
-                    <!-- List of tasks -->
-                    <div class="card mt-2">
-                        <ul class="list-group list-group-flush">
-                        <Task v-for="task in tasks" :task="task" :key="task.id" />
-                        </ul>
-                    </div>
+                    <!-- List of uncompleted tasks -->
+                    <Tasks :tasks="uncompletedTasks" />
+
+                    <!-- show toggle button -->
+
+                    <!-- list of completed tasks -->
+                    <Tasks :tasks="completedTasks" />
                 </div>
             </div>
         </div>
@@ -22,9 +23,9 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { allTasks } from '@/http/task-api';
-import Task from "@/components/tasks/Task.vue";
+import Tasks from "@/components/tasks/Tasks.vue";
 
 const tasks = ref([]); // ref - to make reactive data
 
@@ -32,4 +33,7 @@ onMounted(async () => {
     const { data } = await allTasks();
     tasks.value = data.data;
 });
+
+const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed));
+const completedTasks = computed(() => tasks.value.filter(task => task.is_completed));
 </script>
